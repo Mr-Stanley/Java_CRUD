978e1561bdcd4b63fcd75df95281a1d5e8877a2c
Stanleys world <Stanleyugoo5@gmail.com>
2025-05-16 11:36:42 +0100

    another push

--- src/test/java/com/semicolon/africa/javacrudproject/service/userServiceImplTest.java
+++ src/test/java/com/semicolon/africa/javacrudproject/service/userServiceImplTest.java
@@ -30,36 +30,36 @@ public class userServiceImplTest {
 
     }
 
-//    @Test
-//    public void testThatUserDetailsCanBeUpdated() {
-//        CreateUserRequest createUserRequest = new CreateUserRequest();
-//        createUserRequest.setEmail("Stan@gmail.com");
-//        createUserRequest.setName("Stan");
-//        userService.CreateUser(createUserRequest);
-//        CreateUpdateUserRequest updateUserRequest = new CreateUpdateUserRequest();
-//        updateUserRequest.setEmail("Stanley@gmail.com");
-//        updateUserRequest.setName("Stanley");
-//        userService.UpdateUser(updateUserRequest);
-///*
-//        String UpdatedUser = userService.UpdateUser(updateUserRequest);
-//*/
-//        assertEquals("Stanley@gmail.com",updateUserRequest.getEmail());
-//        assertEquals("Stanley", updateUserRequest.getName());
-//
-//
-//    }
-//    @Test
-//    public void testThatUserDetailsCanBeDeleted() {
-//        CreateUserRequest createUserRequest = new CreateUserRequest();
-//        createUserRequest.setEmail("Stanley081@gmail.com");
-//        createUserRequest.setName("Stanley ugoo");
-//        userService.CreateUser(createUserRequest);
-//        DeleteUserDetails deleteUserDetails = new DeleteUserDetails();
-//        deleteUserDetails.setEmail("Stanley081@gmail.com");
-//        deleteUserDetails.setName("Stanley ugoo");
-//        userService.DeleteUser(deleteUserDetails);
-//        assertNull(userRepository.findByEmail(deleteUserDetails.getEmail()));
-//    }
+    @Test
+    public void testThatUserDetailsCanBeUpdated() {
+        CreateUserRequest createUserRequest = new CreateUserRequest();
+        createUserRequest.setEmail("Stan@gmail.com");
+        createUserRequest.setName("Stan");
+        userService.CreateUser(createUserRequest);
+        CreateUpdateUserRequest updateUserRequest = new CreateUpdateUserRequest();
+        updateUserRequest.setEmail("Stanley@gmail.com");
+        updateUserRequest.setName("Stanley");
+        userService.UpdateUser(updateUserRequest);
+/*
+        String UpdatedUser = userService.UpdateUser(updateUserRequest);
+*/
+        assertEquals("Stanley@gmail.com",updateUserRequest.getEmail());
+        assertEquals("Stanley", updateUserRequest.getName());
+
+
+    }
+    @Test
+    public void testThatUserDetailsCanBeDeleted() {
+        CreateUserRequest createUserRequest = new CreateUserRequest();
+        createUserRequest.setEmail("Stanley081@gmail.com");
+        createUserRequest.setName("Stanley ugoo");
+        userService.CreateUser(createUserRequest);
+        DeleteUserDetails deleteUserDetails = new DeleteUserDetails();
+        deleteUserDetails.setEmail("Stanley081@gmail.com");
+        deleteUserDetails.setName("Stanley ugoo");
+        userService.DeleteUser(deleteUserDetails);
+        assertNull(userRepository.findByEmail(deleteUserDetails.getEmail()));
+    }
 
 }
 
